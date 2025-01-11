99497b310362139f311d8522e34bb7cd2c70430e
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2025-01-11 14:42:51 -0600

    Add append method

--- lib/linked-list.rb
+++ lib/linked-list.rb
@@ -1,7 +1,22 @@
+require_relative 'node'
+
 class LinkedList
   def initialize
     @head = nil
     @tail = nil
     @size = 0
   end
+
+  def append(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next_node = new_node
+      @tail = new_node
+    end
+    @size += 1
+  end
+
 end
